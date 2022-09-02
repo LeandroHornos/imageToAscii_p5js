445a40d9490c7b56ad4551ddb5e640b4ef7e5e67
Lean <leandrohornos@gmail.com>
2022-09-02 15:13:37 -0300

    Agrego imagenes de prueba

--- img-to-ascii.js
+++ img-to-ascii.js
@@ -4,7 +4,13 @@
 */
 
 // Caracteres ordenados de mayor a menor brillo:
-const chars = " Ñ@#W$9876543210?!ABC;:+=-,._ ";
+const density = "Ñ@#W$9876543210?!ABC;:+=-,._ ";
+
+let imagen;
+
+function preload() {
+  imagen = "gatito_preso48.jpg";
+}
 
 function setup() {
   createCanvas(400, 400);
@@ -12,4 +18,5 @@ function setup() {
 
 function draw() {
   background(220);
+  imagen(gloria, 0, 0, witdh, height);
 }
